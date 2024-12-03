913a42a3a2c727cceb71159638aed434f28f7a65
Depermitto <penciller@disroot.org>
2024-12-03 18:13:53 +0100

    use regex matching in 2024/day3

--- 2024/day3/main.jl
+++ 2024/day3/main.jl
@@ -3,8 +3,7 @@ input = readchomp("2024/day3/input.txt")
 # task 1
 let text = input
     re = r"mul\((?<num1>[0-9]+)\,(?<num2>[0-9]+)\)"
-    matches = eachmatch(re, text)
-    total = sum(matches) do m
+    total = sum(eachmatch(re, input)) do m
         parse(Int, m[:num1]) * parse(Int, m[:num2])
     end
     println(total)
@@ -12,41 +11,16 @@ end
 
 # task 2
 let text = input
+    re = r"(mul\((?<num1>[0-9]+)\,(?<num2>[0-9]+)\)|don't|do)"
     mulenabled = true
     total = 0
-    for i in eachindex(text)
-        mul = 0
-        try
-            if text[i:i+3] == "do()"
-                mulenabled = true
-            elseif text[i:i+6] == "don't()"
-                mulenabled = false
-            elseif text[i:i+3] == "mul("
-                isnumeric(text[i+4]) || continue
-                j = i + 5
-                while isnumeric(text[j])
-                    j += 1
-                end
-                if mulenabled
-                    mul = parse(Int, text[i+4:j-1])
-                end
-
-                text[j] == ',' || continue
-
-                isnumeric(text[j+1]) || continue
-                k = j + 2
-                while isnumeric(text[k])
-                    k += 1
-                end
-                if mulenabled
-                    mul *= parse(Int, text[j+1:k-1])
-                end
-
-                text[k] == ')' || continue
-                total += mul
-            end
-        catch e
-            break
+    for m in eachmatch(re, text)
+        if m.match == "don't"
+            mulenabled = false
+        elseif m.match == "do"
+            mulenabled = true
+        elseif mulenabled
+            total += parse(Int, m[:num1]) * parse(Int, m[:num2])
         end
     end
     println(total)
